e7af3641b9ad2b98d74539004cf2f13770b36734
Nicola Farina <50495055+nicola-farina@users.noreply.github.com>
2022-10-26 16:45:54 +0000

    Don't plot CoP

--- assignment_01/hw1_tsid_biped_walking.py
+++ assignment_01/hw1_tsid_biped_walking.py
@@ -21,7 +21,7 @@ print(" Test Walking ".center(conf.LINE_WIDTH, '#'))
 print("".center(conf.LINE_WIDTH, '#'), '\n')
 
 PLOT_COM = 1
-PLOT_COP = 1
+PLOT_COP = 0
 PLOT_FOOT_TRAJ = 0
 PLOT_TORQUES = 0
 PLOT_JOINT_VEL = 0
