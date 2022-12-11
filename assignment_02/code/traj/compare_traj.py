d9e9e721b7cc50a96e07d87182b0b529b0c7b7ff
Nicola Farina <50495055+nicola-farina@users.noreply.github.com>
2022-12-11 10:38:06 +0000

    Add comparing trajectories with or without mu factor

--- assignment_02/code/traj/compare_traj.py
+++ assignment_02/code/traj/compare_traj.py
@@ -14,16 +14,16 @@ def mean_squared_error(vector1: np.ndarray, vector2: np.ndarray) -> float:
 def load_traj(prefix: str) -> Tuple[np.ndarray, np.ndarray]:
     """
     Loads trajectories (states and controls) from .npy files
-    @param prefix: prefix of the file (sel, pen)
+    @param prefix: prefix of the file
     @return: state trajectory (X) and control trajectory (U)
     """
 
-    X = np.load(prefix+"_X.npy")
-    U = np.load(prefix+"_U.npy")
+    X = np.load(prefix + "_X.npy")
+    U = np.load(prefix + "_U.npy")
     return X, U
 
 
-def main():
+def sel_vs_pen():
     """
     Method used to compute the difference between the trajectories found by DPP when using the
     selection matrix (sel) and penalty (pen) methods
@@ -39,5 +39,22 @@ def main():
     print("Joint 1 torque = ", mean_squared_error(X_sel[:, 3], X_pen[:, 3]))
 
 
+def mu_factor_0_vs_10():
+    """
+    Method used to compute the difference between the trajectories found by DPP when using
+    a mu factor of 0 and 10
+    """
+
+    X_0, U_0 = load_traj("mu_factor_0")
+    X_10, U_10 = load_traj("mu_factor_10")
+    print("\n" + " MEAN SQUARED ERROR BETWEEN MU FACTOR 0 AND 10 ".center(60, '*'))
+    print("Joint 1 pos = ", mean_squared_error(X_0[:, 0], X_10[:, 0]))
+    print("Joint 2 pos = ", mean_squared_error(X_0[:, 1], X_10[:, 1]))
+    print("Joint 1 vel = ", mean_squared_error(U_0[:, 0], U_10[:, 0]))
+    print("Joint 2 vel = ", mean_squared_error(X_0[:, 2], X_10[:, 2]))
+    print("Joint 1 torque = ", mean_squared_error(X_0[:, 3], X_10[:, 3]))
+
+
 if __name__ == "__main__":
-    main()
+    # sel_vs_pen()
+    mu_factor_0_vs_10()
