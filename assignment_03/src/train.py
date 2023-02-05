094135a754566407f7e829fa7ea9c3f6164d83ee
Nicola Farina <50495055+nicola-farina@users.noreply.github.com>
2023-02-05 15:28:38 +0000

    Improve training entrypoint

--- assignment_03/src/train.py
+++ assignment_03/src/train.py
@@ -1,31 +1,36 @@
+import orc.assignment_03.src.environment.pendulum as environment
 from orc.assignment_03.src.network import DQNet, Network
-from orc.assignment_03.src.environment.pendulum import SinglePendulum
 
 
 def main():
     hyper_params = DQNet.HyperParams(
-        replay_size=10000,
-        replay_start=1000,
+        replay_size=5000,
+        replay_start=500,
         discount=0.9,
-        max_episodes=100,
+        max_episodes=500,
         max_steps_per_episode=100,
-        steps_for_target_update=1000,
+        steps_for_target_update=100,
         epsilon_start=1.,
         epsilon_decay=0.99,
-        epsilon_min=0.001,
-        batch_size=1024,
-        learning_rate=0.0001
+        epsilon_min=0.01,
+        batch_size=256,
+        learning_rate=0.09,
+        display_every_episodes=25
     )
 
-    num_controls = 11
-    display = False
-    env = SinglePendulum(num_controls=num_controls, display=display)
+    num_joints = 1
+    num_controls = 32
+    name = "single"
 
-    model = Network.get_model(2, num_controls, "trial")
+    env = environment.SinglePendulum(num_controls=num_controls, max_torque=10.)
+    model = Network.get_model(num_joints*2, num_controls, name)
 
     dq = DQNet(model, hyper_params, env)
-    model = dq.train()
-    model.save("models/")
+
+    best_model = dq.train()
+
+    if best_model is not None:
+        best_model.save(f"models/{name}")
 
 
 if __name__ == "__main__":
