9a64016a2d475a575843e24357f3ae259fcf81b8
Nicola Farina <50495055+nicola-farina@users.noreply.github.com>
2023-02-07 13:10:30 +0000

    Improve saving of model

--- assignment_03/src/train.py
+++ assignment_03/src/train.py
@@ -1,20 +1,23 @@
+from datetime import datetime
+
 import orc.assignment_03.src.environment.pendulum as environment
-from dqn.algorithm import DQNet, Network
+from orc.assignment_03.src.dqn.algorithm import DQL
+from orc.assignment_03.src.dqn.network import Network
 
 import numpy.random as random
 
 
 def main():
-    hyper_params = DQNet.HyperParams(
+    hyper_params = DQL.HyperParams(
         replay_size=10000,
-        replay_start=1000,
-        discount=0.9,
-        max_episodes=500,
-        max_steps_per_episode=200,
-        steps_for_target_update=100,
-        epsilon_start=1.,
-        epsilon_decay=0.99,
-        epsilon_min=0.01,
+        replay_start=32,
+        discount=0.99,
+        max_episodes=1,
+        max_steps_per_episode=500,
+        steps_for_target_update=1000,
+        epsilon_start=1.0,
+        epsilon_decay=0.9985,
+        epsilon_min=0.002,
         batch_size=32,
         learning_rate=0.001,
         display_every_episodes=10
@@ -26,15 +29,16 @@ def main():
 
     rng = random.default_rng(seed=42)
 
-    env = environment.SinglePendulum(num_controls=num_controls, max_torque=5., rng=rng)
+    env = environment.SinglePendulum(time_step=0.005, rng=rng)
     model = Network.get_model(num_joints*2, num_controls, name)
 
-    dq = DQNet(model, hyper_params, env, rng=rng)
+    dql = DQL(model, hyper_params, env, rng=rng)
 
-    best_model = dq.train()
+    best_model = dql.train()
 
     if best_model is not None:
-        best_model.save(f"models/{name}")
+        time = datetime.now().strftime("%H%M%S")
+        best_model.save_weights(f"models/{name}_{time}.h5")
 
 
 if __name__ == "__main__":
