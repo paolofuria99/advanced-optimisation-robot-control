b075a777b671afeb23138f2e0799eb256c3cfb35
Nicola Farina <50495055+nicola-farina@users.noreply.github.com>
2023-02-07 16:53:53 +0000

    Use deque instead of custom experience buffer

--- assignment_03/src/dqn/experience.py
+++ assignment_03/src/dqn/experience.py
@@ -1,7 +1,8 @@
+import collections
 from typing import NamedTuple, Tuple
 
-import numpy.typing as npt
 import numpy as np
+import numpy.typing as npt
 
 
 class Experience(NamedTuple):
@@ -17,17 +18,11 @@ class Experience(NamedTuple):
 
 class ExperienceBuffer:
     """
-    This class implements an efficient circular buffer for the experience-replay buffer.
-    We make a custom class and not use something like collections.deque because we want
-    both efficient circular appends (with a maximum size) and efficient random indexing,
-    and the latter is not provided by deque.
+    This class wraps a collections.deque for the experience-replay buffer.
     """
 
     def __init__(self, size: int, rng: np.random.Generator) -> None:
-        self._buffer = np.empty(size, dtype=Experience)
-        self._index = 0
-        self._num_elements = 0
-        self._size = size
+        self._buffer = collections.deque(maxlen=size)
         self._rng = rng
 
     def append(self, experience: Experience) -> None:
@@ -38,10 +33,7 @@ class ExperienceBuffer:
         Args:
             experience: the experience to add.
         """
-        self._buffer[self._index] = experience
-        self._index = (self._index + 1) % self._size
-        if self._num_elements < self._size:
-            self._num_elements += 1
+        self._buffer.append(experience)
 
     def sample(self, quantity: int) -> Tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]:
         """
@@ -58,7 +50,7 @@ class ExperienceBuffer:
             an array of all the costs of taking an action in each experience;
             a boolean mask that says which experiences resulted in a goal state.
         """
-        indices = self._rng.choice(self._num_elements, quantity, replace=False)
+        indices = self._rng.choice(len(self._buffer), quantity, replace=False)
         start_states, actions, next_states, costs, goals = zip(*[self._buffer[idx] for idx in indices])
         return \
             np.array(start_states, dtype=np.float), \
